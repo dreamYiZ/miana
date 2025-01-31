139dfa8b15a8c1e9df887e91b86244b9318754ba
Rentoo <dreamYiZ@outlook.com>
2025-01-31 23:27:56 +0800

    -

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import os
-from ollama_call import scan_image  # 导入 ollama_call 文件中的函数
+from ollama_call import scan_image, ocr_image  # 导入 ollama_call 文件中的函数
 from runtime_util import write_to_runtime_log_file
 
 app = Flask(__name__)
@@ -21,12 +21,13 @@ def scan_images():
         # 将文件名后缀转换为小写，然后进行检查
         if filename.lower().endswith(".jpg") or filename.lower().endswith(".png"):
             image_path = os.path.join(images_folder, filename)
-            result = scan_image(image_path)  # 使用 ollama 扫描图片
+            result = ocr_image(image_path)  # 使用 ollama 扫描图片
+            # result = scan_image(image_path)  # 使用 ollama 扫描图片
             scanned_images.append(result)
             write_to_runtime_log_file(result)
 
 
-    return jsonify({'message': 'Scan completed', 'scanned_images': scanned_images})
+    return jsonify({'message': 'Scan completed', 'scanned_images': 'ok'})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- ollama_call.py
+++ ollama_call.py
@@ -1,15 +1,28 @@
 # ollama_call.py
 import ollama  # 假设 ollama 库已经导入
+import pytesseract
+from PIL import Image
+
+# 识别图片中的文字，采用 pytesseract
+def ocr_image(image_path):
+    try:
+        # 打开图片文件
+        img = Image.open(image_path)
+        # 使用 pytesseract 识别图片中的文字
+        text = pytesseract.image_to_string(img, lang='chi_sim')  # 使用中文识别
+        return text
+    except Exception as e:
+        return {'error': str(e)}
 
 def scan_image(image_path):
     # 使用 ollama 模型识别图片中的文字并生成描述
     try:
         res = ollama.chat(
-            model="llava",
+            model="llava:7b",
             messages=[
                 {
                     'role': 'user',
-                    'content': 'Describe this image:',
+                    'content': '识别其中的文字，返回中文文字:',
                     'images': [image_path]  # 使用传入的 image_path
                 }
             ]
